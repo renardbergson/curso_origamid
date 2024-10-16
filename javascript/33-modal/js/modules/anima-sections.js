0192e3b45806f9d139dbde7d1f8add10de6355a3
renardbergson <renardrock@gmail.com>
2024-10-15 23:07:01 -0300

    inserindo aula 33, curso javascript

---
+++ javascript/33-modal/js/modules/anima-sections.js
@@ -0,0 +1,26 @@
+const sections = document.querySelectorAll("[data-anime='fade']");
+import nomeClasse from "../script.js";
+
+function animaSections() {
+  const porcentagemAlturaTela = window.innerHeight * 0.7; 
+  // 70% da altura interna da janela
+  // quanto maior a porcentagem, menor será o espaço até o topo do elemento
+
+  function animaScroll() {
+    sections.forEach((item, index) => {
+      const topo = item.getBoundingClientRect().top;
+      const distanciaDesejada = (topo - porcentagemAlturaTela) < 0;
+
+      if (distanciaDesejada) {
+        item.classList.add(nomeClasse);
+      } else {
+        item.classList.remove(nomeClasse);
+      }
+    })
+  }
+
+  window.addEventListener("scroll", animaScroll);
+  animaScroll(); // animar a primeira, antes mesmo do scroll
+}
+
+export default animaSections;
---
+++ javascript/33-modal/js/script.js
@@ -0,0 +1,18 @@
+// =================== MODAL ===================
+// Um modal é um elemento de interface de usuário (UI) que aparece sobre o conteúdo principal da página ou aplicação e exige interação do usuário antes de retornar à interface normal. É geralmente usado em janelas pop-up ou caixas de diálogo para alertar o usuário, confirmar ações ou coletar informações.
+
+import mostrarDescricaoAnimal from "./modules/mostrar-descricao-animais.js";
+import animaSections from "./modules/anima-sections.js";  
+import scrollSuave from "./modules/scroll-suave.js";
+import initAccordion from "./modules/accordion.js";
+import handleModal from "./modules/handleModal.js";
+
+const nomeClasse = "ativo"; // nome de classe ulitazada nos módulos
+
+export default nomeClasse; 
+
+mostrarDescricaoAnimal(); 
+animaSections();
+scrollSuave();
+initAccordion();
+handleModal();
