108f2b90efd9a34f037b3df4ef79ef9adf6c8b7d
renardbergson <renardrock@gmail.com>
2024-11-27 01:30:48 -0300

    inserindo aula 27, curso typescript

---
+++ typescript/27_ts-config/src/index.ts
@@ -0,0 +1,3 @@
+// ==================== TS-CONFIG ====================
+// Nesta aula, serão abordadas as configurações mais comuns do arquivo tsconfig.json
+// Todas as anotações referentes a esta aula estarão no arquivo tsconfig-aula.json
